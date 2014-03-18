8e615032d47c2eaba06aeb5b7082378f6852d77a
Kaitlin Huben <kaitlin.huben@gmail.com>
2014-03-18 00:34:40 -0400

    Completed framework for projects index

---
+++ js/custom-isotope.js
@@ -0,0 +1,54 @@
+(function ($, window, document, undefined) {
+/* ---------------------------------------------------------------------- */
+	/*	Cats Filter
+	/* ---------------------------------------------------------------------- */ 
+	
+	var $catsfilter 		= $('.categories-filter');
+
+	// Copy categories to item classes
+	$catsfilter.find('a').click(function() {
+		var currentOption = $(this).attr('data-filter');
+		$(this).parent().parent().find('a').removeClass('current');
+		$(this).addClass('current');
+	});	
+
+	/* ---------------------------------------------------------------------- */
+	/*	Portfolio
+	/* ---------------------------------------------------------------------- */ 
+	
+	// Needed variables
+	var $plist	 	= $('#projects-list');
+	var $pfilter 		= $('#projects-filter');
+		
+	// Run Isotope  
+	$plist.isotope({
+		filter				: '*',
+		layoutMode   		: 'masonry',
+		animationOptions	: {
+		duration			: 750,
+		easing				: 'linear'
+	   }
+	});	
+	
+	// Isotope Filter 
+	$pfilter.find('a').click(function(){
+	  var selector = $(this).attr('data-filter');
+		$plist.isotope({ 
+		filter				: selector,
+		animationOptions	: {
+		duration			: 750,
+		easing				: 'linear',
+		queue				: false,
+	   }
+	  });
+	  return false;
+	});	
+	
+	// Portfolio image animation 
+	$plist.find('img').adipoli({
+		'startEffect' 	: 'transparent',
+		'hoverEffect' 	: 'boxRandom',
+		'imageOpacity' 	: 0.6,
+		'animSpeed' 	: 100,
+	});
+})(window.jQuery, this, document);
