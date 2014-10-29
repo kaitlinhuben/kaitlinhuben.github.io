831ffaebb633757407aeab80c0abf32245ca409d
Kaitlin Huben <kaitlin.huben@gmail.com>
2014-10-29 14:47:03 -0400

    Switched to downward scroll instead of side slide

--- DanceInNYC/js/animations.js
+++ DanceInNYC/js/animations.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-	console.log('ready!');
+$(document).ready(function() {	
+	// get all section elements
+	$navbar = $('#navbar');
+	$zeroth = $('#zeroth');
+	$first = $('#first');
+	$second = $('#second');
+	
+	// set variables
+	SCROLL_TIME = 1000;
+	navHeight = $navbar.outerHeight();
+	
+	// set up all scrolls
+	$('#go-to-1').click(function() {
+		$('body').animate({
+			scrollTop: $first.position().top - navHeight
+		  }, SCROLL_TIME);
+	});
+	
+	//TODO fade in first section?
 });
