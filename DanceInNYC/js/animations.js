75aa1a6acb4893c040baccb48ec29d0a865bcc41
Kaitlin Huben <kaitlin.huben@gmail.com>
2014-11-02 16:35:23 -0500

    More content and animations, +icon

--- DanceInNYC/js/animations.js
+++ DanceInNYC/js/animations.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {	
 	// get all section elements
 	$navbar = $('#navbar');
+	$navbarlogo = $('#logo');
 	$zeroth = $('#zeroth');
 	$first = $('#first');
 	$second = $('#second');
+	$third = $('#third');
+	$fourth = $('#fourth');
+	$footer = $('#footer');
 	
 	// set variables
 	SCROLL_TIME = 1000;
+	FADE_TIME = 750;
 	navHeight = $navbar.outerHeight();
 	
 	// set up all scrolls
@@ -25,6 +30,38 @@ $(document).ready(function() {
 			scrollTop: $second.position().top - navHeight
 		  }, SCROLL_TIME);
 	});
+	$('#go-to-3').click(function() {
+		$('body').animate({
+			scrollTop: $third.position().top - navHeight
+		  }, SCROLL_TIME);
+	});
+	$('#go-to-4').click(function() {
+		$('body').animate({
+			scrollTop: $fourth.position().top - navHeight
+		  }, SCROLL_TIME);
+	});
+
+	// hide first section and then fade back in
+	$navbarlogo.css('opacity', 0);
+	$zeroth.css('opacity', 0);
+	$footer.css('opacity', 0);
+	$navbarlogo.animate({
+		opacity: 1
+	  }, 
+	  // time
+	  FADE_TIME, 
+	  //callback
+	  function() {
+		$zeroth.animate({
+			opacity: 1
+		}, 
+		FADE_TIME,
+		function (){
+			$footer.animate({
+				opacity: 1
+			},
+			FADE_TIME)
+		})
+	  });
 	
-	//TODO fade in first section?
 });
