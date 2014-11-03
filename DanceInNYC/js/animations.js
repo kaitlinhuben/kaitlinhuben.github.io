e119ec9c995d874e3d69606a763550198198e2f2
Kaitlin Huben <kaitlin.huben@gmail.com>
2014-11-02 20:13:57 -0500

    Semi-final draft

--- DanceInNYC/js/animations.js
+++ DanceInNYC/js/animations.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {	
+	// ----------------------------------------------------
 	// get all section elements
+	// ----------------------------------------------------
 	$navbar = $('#navbar');
 	$navbarlogo = $('#logo');
 	$zeroth = $('#zeroth');
+	$photobanner = $('#photo-banner');
 	$first = $('#first');
 	$second = $('#second');
 	$third = $('#third');
 	$fourth = $('#fourth');
 	$footer = $('#footer');
 	
+	// ----------------------------------------------------
 	// set variables
+	// ----------------------------------------------------
 	SCROLL_TIME = 1000;
 	FADE_TIME = 750;
 	navHeight = $navbar.outerHeight();
 	
-	// set up all scrolls
+	// ----------------------------------------------------
+	// set up all scroll handlers
+	// ----------------------------------------------------
 	$('.go-to-top').click(function() {
 		$('body').animate({
 			scrollTop: $zeroth.position().top - navHeight
@@ -41,29 +48,57 @@ $(document).ready(function() {
 		  }, SCROLL_TIME);
 	});
 
-	// hide first section and then fade back in
+	// ----------------------------------------------------
+	// hide top sections
+	// ----------------------------------------------------
 	$navbarlogo.css('opacity', 0);
 	$zeroth.css('opacity', 0);
 	$footer.css('opacity', 0);
-	$navbarlogo.animate({
-		opacity: 1
-	  }, 
-	  // time
-	  FADE_TIME, 
-	  //callback - fade top section in
-	  function() {
-		$zeroth.animate({
+	$photobanner.css('opacity', 0);
+	// ----------------------------------------------------
+	// fade top sections back in
+	// ----------------------------------------------------
+	$navbarlogo.animate(
+		{
 			opacity: 1
 		}, 
-		//time
-		FADE_TIME,
-		// callback - fade footer in
-		function (){
-			$footer.animate({
+		// time
+		FADE_TIME, 
+		//callback - fade zeroth intro section in
+		function() {
+			animateZerothSection();
+		}
+	);
+	
+	// helper function - navbar's callback
+	function animateZerothSection() {
+		$zeroth.animate(
+			{
 				opacity: 1
 			},
+			// time
+			FADE_TIME,
+			// callback - fade footer and photo in
+			function (){
+				animateFooterAndPhoto();
+			}
+		)
+	}
+	  
+	// helper function - zeroth section's callback
+	function animateFooterAndPhoto() {
+		// fade in footer
+		$footer.animate(
+			{
+				opacity: 1
+			}, 
+			FADE_TIME);
+		// fade in photo
+		$photobanner.animate(
+			{
+				opacity: 1
+			}, 
 			FADE_TIME)
-		})
-	  });
+	  }
 	
 });
